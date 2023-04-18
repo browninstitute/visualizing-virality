e694209fd669a9bd03b928def2f2b3013272e34d
Samia Menon <55510385+samia-mnn@users.noreply.github.com>
2023-04-18 16:02:58 -0400

    Update network1.js

--- src/sections/network_p5/network1.js
+++ src/sections/network_p5/network1.js
@@ -433,11 +433,11 @@ function sketch(fp5) {
           }
 
           //then brute force go away if too long
-          let timeSince = fp5.log(this.time)*120 - adjFrame;
+        /*  let timeSince = fp5.log(this.time)*120 - adjFrame;
           if (timeSince < -500 && this.isFirst != 'first')
           {
             return;
-          }
+          }*/
         
             let scaler = (fp5.int(followerMap.get(this.name))/4000+10)/load_factor;
         
